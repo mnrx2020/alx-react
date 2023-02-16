fa823e5d2c5eb1d0e74929c9999b0477e3086de6
James Mbere <99666930+mnrx2020@users.noreply.github.com>
2023-02-16 14:40:54 +0300

    Create 0-fromjs.js

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,10 @@
+/*
+function getImmutableObject that accepts object as a parameter and
+converts it into an immutable Map using fromJS of the Immutable.js library
+*/
+import { fromJS } from 'immutable';
+
+export default function getImmutableObject(object) {
+  const newObj = fromJS(object);
+  return newObj;
+}
